96c9f8eb51c5e2b938fda483b59b1c43d1737211
Your Name <you@example.com>
2024-11-26 22:15:52 +0330

    resolved bugs

--- my-app/app/component/api/api.ts
+++ my-app/app/component/api/api.ts
@@ -5,16 +5,10 @@ const getBaseUrl = (): string => {
   } else {
     // Client-side
     const domain = window.location.hostname;
-    if (
-      domain ===
-      "https://e-commerce-next-ts-git-master-nimas-projects-7884e4b5.vercel.app"
-    ) {
-      return "https://e-commerce-next-ts-git-master-nimas-projects-7884e4b5.vercel.app";
-    } else if (
-      domain ===
-      "https://e-commerce-next-ts-git-master-nimas-projects-7884e4b5.vercel.app"
-    ) {
-      return "https://e-commerce-next-ts-git-master-nimas-projects-7884e4b5.vercel.app";
+    if (domain === "https://e-commerce-next-ts-two.vercel.app") {
+      return "https://e-commerce-next-ts-two.vercel.app";
+    } else if (domain === "https://e-commerce-next-ts-two.vercel.app") {
+      return "https://e-commerce-next-ts-two.vercel.app";
     } else {
       return "http://localhost:3001"; // Default for local development
     }
