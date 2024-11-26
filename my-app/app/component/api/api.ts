929987e742fabfa9e99e351e33bdc2b50ec628e3
Your Name <you@example.com>
2024-11-26 23:41:30 +0330

    resolved bugs

--- my-app/app/component/api/api.ts
+++ my-app/app/component/api/api.ts
@@ -1,24 +1,5 @@
-const getBaseUrl = (): string => {
-  if (typeof window === "undefined") {
-    // Server-side
-    return (
-      process.env.API_BASE_URL ||
-      "https://clean-successful-tachometer.glitch.me"
-    );
-  } else {
-    // Client-side
-    const domain = window.location.hostname;
-    if (domain === "https://clean-successful-tachometer.glitch.me") {
-      return "https://clean-successful-tachometer.glitch.me";
-    } else if (domain === "https://clean-successful-tachometer.glitch.me") {
-      return "https://clean-successful-tachometer.glitch.me";
-    } else {
-      return "http://localhost:3001"; // Default for local development
-    }
-  }
-};
 
-const baseUrl = getBaseUrl();
+const baseUrl = "https://clean-successful-tachometer.glitch.me";
 
 export const languages = `${baseUrl}/language`;
 export const languagesSelected = `${baseUrl}/selected`;
