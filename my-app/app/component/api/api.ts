8d5ffed6dd5c34678d4b5a12eae391b25bbdeb21
Your Name <you@example.com>
2024-11-28 01:28:33 +0330

    resolved bug

--- my-app/app/component/api/api.ts
+++ my-app/app/component/api/api.ts
@@ -1,4 +1,7 @@
-const baseUrl = "/api";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://your-vercel-app.vercel.app/api" // Vercel API endpoint
+    : "http://localhost:3001"; // Local development API
 
 export const languages = `${baseUrl}/language`;
 export const languagesSelected = `${baseUrl}/selected`;
